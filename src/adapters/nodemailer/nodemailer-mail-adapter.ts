4b8727b801560f8563249c6a1d5f72379b1e9173
Guilherme Raposo <gui.raposo36@gmail.com>
2022-05-06 12:09:06 -0300

    feat: finished application

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "456e3b421255cc",
+    pass: "9c48ac1bdb6c79",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Guilherme Raposo <gui.raposo36@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
---
+++ src/use-cases/submit-feedback-use-case.test.ts
@@ -0,0 +1,56 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+// spes = espiões
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+);
+
+describe("Submit feedback", () => {
+  it("should be able submit a feedback", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,16f1b65f1b56f1db56",
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit feedback without type", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,16f1b65f1b56f1db56",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit feedback without comment", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "",
+        screenshot: "data:image/png;base64,16f1b65f1b56f1db56",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit feedback with an invalid screenshot", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "test.jpg",
+      })
+    ).rejects.toThrow();
+  });
+});
